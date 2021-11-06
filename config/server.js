a9f73090fb089dbe0332ba480f01c2cf5cfd94a9
j-five <sergiy.kunnik.private@gmail.com>
2021-11-07 01:16:20 +0200

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fc451b2349473112e626810fe2922445'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '27e49514-1587-405a-955b-e25fc327ac3e'
+};
