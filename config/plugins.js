a57e8ed77dcb0d868c193104acf6a0f9156c1f5f
j-five-ekreative <sergiy.kunnik@ekreative.com>
2022-01-25 21:29:57 +0200

    aws s3

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,26 @@
-module.exports = {
-  //
-  graphql: {
-    endpoint: "/graphql",
-    shadowCRUD: true,
-    playgroundAlways: false,
-    depthLimit: 7,
-    amountLimit: 100,
-    apolloServer: {
-      tracing: false,
+module.exports = ({ env }) => {
+  // console.log(`env('AWS_ACCESS_KEY_ID') => `, env('AWS_ACCESS_KEY_ID'))
+  return {
+    graphql: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
     },
-  },
+    upload: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: "eu-central-1",
+        params: {
+          Bucket: "lof-upload",
+        },
+      },
+    },
+  };
 };
